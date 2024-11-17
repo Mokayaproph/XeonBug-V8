87f5eabdd13d6ad53d493276fa04af107873b259
Mokayaproph <143901742+Mokayaproph@users.noreply.github.com>
2024-11-18 00:47:32 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
-global.botname = 'XeonBug-V8' //name of the bot
+global.ownernumber = '254742427165'  //creator number
+global.ownername = 'NI PROPH' //owner name
+global.botname = 'proph bugs' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
@@ -41,7 +41,7 @@ global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti91 = false //auto block +91 
-global.autoswview = false //auto view status/story
+global.autoswview = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
